23bf12a680bbf7ad3fd0cb26bcca3b4f4ea421f1
DB1\alan.secundo <alan.secundo@db1.com.br>
2021-05-14 20:19:40 -0300

    Alteração no arquivo App.js, alterado texto principal da aplicação

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Aula ADS 2021 - A turma é massa, 6 são fera!</Text>
       <StatusBar style="auto" />
     </View>
   );
